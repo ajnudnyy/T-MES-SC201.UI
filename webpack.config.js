fd9cf5a1e2189c2e487cd2278ea25d9a4adeffe3
ajnudnyy <gjeunney@163.com>
2017-11-22 17:35:47 +0800

    修改注释

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 /**
   * @file mock文件
-  * @author dva<yingfeng.ni@gmail.com>
+  * @author dva<gjeunney@163.com>
   */
 const fs = require('fs');
 const path = require('path');
@@ -21,38 +21,6 @@ module.exports = function (webpackConfig, env) {
         webpackConfig.babel.plugins.push('dev-expression');
     }
 
-    // 尼玛处理了好久！！！！！不懂webpack 胡乱注释
-    // 使用css modules 确实非常爽 但是TMD 谁能帮我在使用 CSS Modules的情况下
-    // 解决字体文件 包括对百度域名屏蔽的问题 只能舍弃 CSS Modules 来进行解决
-    // Support CSS Modules
-    // Parse all less files as css module.
-    // webpackConfig.module.loaders.forEach(function (loader, index) {
-    //     if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.less$') > -1) {
-    //         loader.include = /node_modules/;
-    //         loader.test = /\.less$/;
-    //     }
-
-    //     if (loader.test.toString() === '/\\.module\\.less$/') {
-    //         loader.exclude = /node_modules/;
-    //         loader.test = /\.less$/;
-    //     }
-
-    //     if (typeof loader.test === 'function' && loader.test.toString().indexOf('\\.css$') > -1) {
-    //         loader.include = /node_modules/;
-    //         loader.test = /\.css$/;
-    //     }
-
-    //     if (loader.test.toString() === '/\\.module\\.css$/') {
-    //         loader.exclude = /node_modules/;
-    //         loader.test = /\.css$/;
-    //     }
-
-    // });
-
-    // webpackConfig.babel.plugins.push(['import', {
-    //     libraryName: 'antd',
-    //     style: 'css'
-    // }]);
     webpackConfig.resolve.alias = {
         components: `${__dirname}/src/components`,
         feature: `${__dirname}/src/components/feature`,
