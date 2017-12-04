58ec7c7f7cbcece9accd76d96b50f5caf1783f0a
白航铭 <gjeunney@163.com>
2017-12-04 17:33:41 +0800

    原型图 && HTML

---
+++ 松成框架原型_2017.11.17/files/品质报表/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,[j],k,_(l,m,n,o,p,q,r,_(),s,_(t,u,v,w,x,_(y,z,A,B),C,null,D,w,E,w,F,G,H,null,I,J,K,L,M,N,O,J),P,_(),Q,_(),R,_(S,[_(T,U,V,W,X,Y,n,Z,ba,Z,bb,bc,s,_(),P,_(),bd,_(),be,[],bf,g)])),bg,_(),bh,_(bi,_(bj,bk)));}; 
+var b="url",c="品质报表.html",d="generationDate",e=new Date(1512379156249),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="variables",j="OnLoadVariable",k="page",l="packageId",m="23d95a731ecd4333bc833e98a28d6e9f",n="type",o="Axure:Page",p="name",q="品质报表",r="notes",s="style",t="baseStyle",u="627587b6038d43cca051c114ac41ad32",v="pageAlignment",w="near",x="fill",y="fillType",z="solid",A="color",B=0xFFFFFFFF,C="image",D="imageHorizontalAlignment",E="imageVerticalAlignment",F="imageRepeat",G="auto",H="favicon",I="sketchFactor",J="0",K="colorStyle",L="appliedColor",M="fontName",N="Applied Font",O="borderWidth",P="adaptiveStyles",Q="interactionMap",R="diagram",S="objects",T="id",U="18ef39a83e6344adb62fcc3d7241e72c",V="label",W="",X="friendlyType",Y="组合",Z="layer",ba="styleType",bb="visible",bc=true,bd="imageOverrides",be="objs",bf="propagate",bg="masters",bh="objectPaths",bi="18ef39a83e6344adb62fcc3d7241e72c",bj="scriptId",bk="u3757";
+return _creator();
+})());
